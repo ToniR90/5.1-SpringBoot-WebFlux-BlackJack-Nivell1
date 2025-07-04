e9d0128b34dc994d5b4f3a1e5c4d992f930f9236
ToniR90 <romy90.t@gmail.com>
2025-07-04 17:46:16 +0200

    card model created

--- blackjack/src/main/java/com/blackjack/api/model/Card.java
+++ blackjack/src/main/java/com/blackjack/api/model/Card.java
@@ -1,4 +1,53 @@
 package com.blackjack.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Card {
+
+    private Suit suit;
+    private Rank rank;
+
+    public enum Suit{
+        HEARTS , DIAMONDS , CLUBS , SPADES
+    }
+
+    public enum Rank{
+        TWO(2) , THREE(3) , FOUR(4) , FIVE(5) ,
+        SIX(6) , SEVEN(7) , EIGHT(8) , NINE(9) ,
+        TEN(10) , JACK(10) , QUEEN(10) , KING(10) ,
+        ACE(11);
+
+        private final int value;
+
+        Rank(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public boolean isAce(){
+            return this == ACE;
+        }
+    }
+
+    public int getCardValue(){
+        return rank.getValue();
+    }
+
+    public boolean isAce(){
+        return rank.isAce();
+    }
+
+    @Override
+    public String toString() {
+        return rank.name() + " of " + suit.name() + "\n" +
+                "Value: " + rank.getValue();
+    }
 }
