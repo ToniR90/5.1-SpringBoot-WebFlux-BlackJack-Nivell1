be34a30e0d73671af12bfe5f58e0cf4779a4df1a
ToniR90 <romy90.t@gmail.com>
2025-07-05 00:06:11 +0200

    deck model implementation

--- blackjack/src/main/java/com/blackjack/api/game/Deck.java
+++ blackjack/src/main/java/com/blackjack/api/game/Deck.java
@@ -1,4 +1,43 @@
 package com.blackjack.api.game;
 
+import com.blackjack.api.model.Card;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+
+    private final List<Card> deck = new ArrayList<>();
+
+    public Deck() {
+        for(Card.Suit suit : Card.Suit.values()) {
+            for(Card.Rank rank : Card.Rank.values()){
+                deck.add(new Card(suit , rank));
+            }
+        }
+    }
+
+    public List<Card> getDeck(){
+        return deck;
+    }
+
+    public void shuffle(){
+        Collections.shuffle(deck);
+    }
+
+    public Card drawCard(){
+        if(deck.isEmpty()){
+            throw new IllegalStateException("There's no more cards in the deck");
+        }
+        return deck.remove(0);
+    }
+
+    public int remainingCards() {
+        return deck.size();
+    }
+
+    public boolean isEmpty(){
+        return deck.isEmpty();
+    }
 }
