0cef3a6f38948e1eb0459afe35de611be5ef6689
ToniR90 <romy90.t@gmail.com>
2025-07-04 14:55:04 +0200

    Game model update

--- blackjack/src/main/java/com/blackjack/api/model/Game.java
+++ blackjack/src/main/java/com/blackjack/api/model/Game.java
@@ -1,17 +1,20 @@
 package com.blackjack.api.model;
 
 import jakarta.persistence.Id;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @EqualsAndHashCode
 @Document(collection = "games")
 public class Game {
@@ -19,12 +22,19 @@ public class Game {
     @Id
     private String id;
 
+    @NotNull(message = "Player ID cannot be null")
     private Long playerId;
+
+    @Builder.Default
     private List<Card> playerCards = new ArrayList<>();
+
+    @Builder.Default
     private List<Card> dealerCards = new ArrayList<>();
+
     private GameStatus gameStatus;
     private int playerFinalScore;
     private int dealerFinalScore;
+    private LocalDateTime playedAt = LocalDateTime.now();
 
     public enum GameStatus{
         WIN , LOSS , DRAW
@@ -52,7 +62,7 @@ public class Game {
                 "Player Id: " + playerId + "\n" +
                 "Player Cards: " + playerCards + "\n" +
                 "Dealer Cards: " + dealerCards + "\n" +
-                "Game Status: " + gameStatus + "\n" +
+                "Game Status: " + (gameStatus != null ? gameStatus.name() : "NOT_SET") + "\n" +
                 "Player final score: " + playerFinalScore + "\n" +
                 "Dealer final score: " + dealerFinalScore;
     }
