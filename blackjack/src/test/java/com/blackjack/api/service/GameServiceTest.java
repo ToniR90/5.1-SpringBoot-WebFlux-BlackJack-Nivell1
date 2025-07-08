90f6793c7c2acfc1b9491db04e7e183c2d550c15
ToniR90 <romy90.t@gmail.com>
2025-07-08 11:22:18 +0200

    Base configuration on GameServiceTest

--- blackjack/src/test/java/com/blackjack/api/service/GameServiceTest.java
+++ blackjack/src/test/java/com/blackjack/api/service/GameServiceTest.java
@@ -1,10 +1,30 @@
 package com.blackjack.api.service;
 
+import com.blackjack.api.game.GameLogic;
+import com.blackjack.api.repository.mongo.GameRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class GameServiceTest {
+
+    @Mock
+    private GameRepository gameRepository;
+
+    @Mock
+    private GameLogic gameLogic;
+
+    @Mock
+    private PlayerService playerService;
+
+    @InjectMocks
+    private GameService gameService;
+
 
     @Test
     void startNewGame() {
