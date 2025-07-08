046e33dde51de05e567df7b02ecfdc82398ecc12
ToniR90 <romy90.t@gmail.com>
2025-07-08 12:24:38 +0200

    startNewGame test implementated

--- blackjack/src/test/java/com/blackjack/api/service/GameServiceTest.java
+++ blackjack/src/test/java/com/blackjack/api/service/GameServiceTest.java
@@ -1,14 +1,19 @@
 package com.blackjack.api.service;
 
 import com.blackjack.api.game.GameLogic;
+import com.blackjack.api.model.Game;
+import com.blackjack.api.model.Player;
 import com.blackjack.api.repository.mongo.GameRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class GameServiceTest {
@@ -28,6 +33,22 @@ class GameServiceTest {
 
     @Test
     void startNewGame() {
+        Long playerId = 1L;
+        Game mockGame = Game.builder()
+                .playerId(playerId)
+                .gameStatus(Game.GameStatus.IN_PROGRESS)
+                .build();
+
+
+        when(playerService.getPlayerById(playerId)).thenReturn(Mono.just(new Player()));
+        when(gameRepository.save(any(Game.class))).thenReturn(Mono.just(mockGame));
+        when(gameLogic.isBlackjack(anyList())).thenReturn(false);
+        when(gameLogic.calculateHandValue(anyList())).thenReturn(15);
+
+        StepVerifier.create(gameService.startNewGame(playerId))
+                .expectNext(mockGame)
+                .verifyComplete();
+
     }
 
     @Test
